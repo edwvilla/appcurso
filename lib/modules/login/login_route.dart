2a7928554df7ad029040280fb205314e97dda2b2
Edwin Villa <villaceniceros@gmail.com>
2024-02-09 18:10:36 -0700

    added padding

--- lib/modules/login/login_route.dart
+++ lib/modules/login/login_route.dart
@@ -11,41 +11,48 @@ class LoginRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(width: double.infinity),
-            const Text(
-              "Welcome back!",
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              const SizedBox(width: double.infinity),
+              const Text(
+                "Welcome back!",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const Text(
-              "Login to your account",
-              style: TextStyle(
-                fontSize: 14,
+              const Text(
+                "Login to your account",
+                style: TextStyle(
+                  fontSize: 14,
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            SvgPicture.asset(
-              "assets/login.svg",
-              width: 180,
-              height: 180,
-            ),
+              const SizedBox(height: 20),
+              SvgPicture.asset(
+                "assets/login.svg",
+                width: 180,
+                height: 180,
+              ),
+              const SizedBox(height: 20),
+              // INPUTS
 
-            // INPUTS
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.greenAccent),
+                  ),
+                ),
+              ),
 
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(),
-            ),
-
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(),
-            ),
-          ],
+              TextField(
+                controller: passwordController,
+                decoration: InputDecoration(),
+              ),
+            ],
+          ),
         ),
       ),
     );
