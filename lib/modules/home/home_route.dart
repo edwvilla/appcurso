8a10847ae173351b007fdec887942f9132827d14
Edwin Villa <villaceniceros@gmail.com>
2024-02-12 13:01:08 -0700

    add sesion lunes

---
+++ lib/modules/home/home_route.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeRoute extends StatelessWidget {
+  const HomeRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Home Route"),
+      ),
+    );
+  }
+}
--- lib/modules/login/login_route.dart
+++ lib/modules/login/login_route.dart
@@ -1,3 +1,5 @@
+import 'package:appcurso/modules/home/home_route.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -6,6 +8,37 @@ class LoginRoute extends StatelessWidget {
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  Future<void> signIn(BuildContext context) async {
+    // validar los datos
+    final String email = emailController.text;
+    final String password = passwordController.text;
+
+    if (email.isEmpty || password.isEmpty) {
+      print("email o password no recibidos");
+      return;
+    }
+
+    // hacer la peticion
+    const String baseUrl =
+        "https://polar-plains-89142-ae7bf2bd796a.herokuapp.com/";
+    const String path = "api/auth/local";
+    try {
+      final result = await Dio().post(
+        "$baseUrl$path",
+        data: {
+          "identifier": email,
+          "password": password,
+        },
+      );
+      print(result);
+
+      if (result.data["jwt"] != null) {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => HomeRoute()));
+      }
+    } catch (e) {}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,20 +71,119 @@ class LoginRoute extends StatelessWidget {
               const SizedBox(height: 20),
               // INPUTS
 
-              TextField(
+              CustomTextField(
                 controller: emailController,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.greenAccent),
+                label: "Enter your email",
+                icon: Icons.person_outline,
+              ),
+
+              const SizedBox(height: 20),
+
+              CustomTextField(
+                controller: passwordController,
+                label: "Enter your password",
+                icon: Icons.lock_outlined,
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 40),
+
+              SizedBox(
+                width: double.infinity,
+                height: 70,
+                child: ElevatedButton(
+                  onPressed: () => signIn(context),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    "Sign In",
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-
-              TextField(
-                controller: passwordController,
-                decoration: InputDecoration(),
-              ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    required this.icon,
+    this.obscureText = false,
+  });
+
+  final TextEditingController controller;
+  final String label;
+  final IconData icon;
+  final bool obscureText;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool showPassword = false;
+
+  @override
+  void initState() {
+    showPassword = widget.obscureText;
+    super.initState();
+  }
+
+  void toggleShowPassword() {
+    showPassword = !showPassword;
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 2,
+      child: TextField(
+        controller: widget.controller,
+        obscureText: widget.obscureText && showPassword,
+        decoration: InputDecoration(
+          label: Text(widget.label),
+          prefixIcon: Icon(widget.icon),
+          prefixIconColor: MaterialStateColor.resolveWith((states) =>
+              states.contains(MaterialState.focused)
+                  ? Colors.green
+                  : Colors.grey),
+          suffixIcon: (widget.obscureText)
+              ? InkWell(
+                  onTap: toggleShowPassword,
+                  child: Icon(
+                    showPassword ? Icons.visibility : Icons.visibility_off,
+                  ),
+                )
+              : null,
+          suffixIconColor: MaterialStateColor.resolveWith(
+            (states) => states.contains(MaterialState.focused)
+                ? Colors.green
+                : Colors.grey,
+          ),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green),
           ),
         ),
       ),
